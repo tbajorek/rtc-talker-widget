9454851a1b1a0a7c61f737aeebb9dba912446ea7
tbajorek <tbajorek3@gmail.com>
2019-01-02 01:01:06 +0100

    Fix a bug

--- src/components/InitModal/DepartmentSelect/index.js
+++ src/components/InitModal/DepartmentSelect/index.js
@@ -11,7 +11,8 @@ const Option = Select.Option;
 const DepartmentSelect = ({
   departments, selectedDepartment, changeDepartment, validation,
 }) => {
-  const options = ("toObject" in departments && typeof departments.toObject === 'function') ? Object.entries(departments.toObject()).map(([id, name]) => <Option key={id} value={id}>{name}</Option>) : [];
+  const departmentsToIterate = ("toObject" in departments && typeof departments.toObject === 'function') ? departments.toObject() : departments;
+  const options = Object.entries(departmentsToIterate).map(([id, name]) => <Option key={id} value={id}>{name}</Option>);
   const childProps = {
     showSearch: true,
     style: {
